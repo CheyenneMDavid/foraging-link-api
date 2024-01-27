b5700e746ec5cb7b1ff094eff86ca55a874bb34c
Chey <ctenmanikdavid@gmail.com>
2024-01-27 21:00:23 +0000

    Added JWT, root route, pagination and humanized date format

--- comments/serializers.py
+++ comments/serializers.py
@@ -5,6 +5,7 @@ Much of the code in this file is copied from the drf-api walkthrough projects
 with Code Institute.
 """
 
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Comment
 
@@ -26,6 +27,8 @@ class CommentSerializer(serializers.ModelSerializer):
     # owner.
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_image = serializers.ReadOnlyField(source="owner.profile.image.url")
+    created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         """
@@ -34,6 +37,14 @@ class CommentSerializer(serializers.ModelSerializer):
         """
         request = self.context["request"]
         return request.user == obj.owner
+
+    # Using Django's humanize module to display when a comment was made in a
+    # more user friendly way
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         """
