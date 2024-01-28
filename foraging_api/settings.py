a6cdfade36fa893c6eec81db76b747ade79aec12
Chey <ctenmanikdavid@gmail.com>
2024-01-28 04:33:24 +0000

    Update DEBUG to environmental variable

--- foraging_api/settings.py
+++ foraging_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 if os.path.exists("env.py"):
@@ -60,13 +61,25 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
-    "8000-cheyennemda-foraginglin-6vpdcr5t4rd.ws-eu107.gitpod.io",
-    "foraging-link-1601764f3844.herokuapp.com",
+    # Using an environmental variable, lessening the amount of unwanted
+    # traffic.
+    os.environ.get("ALLOWED_HOST"),
     "localhost",
 ]
+
+if "CLIENT_ORIGIN" in os.environ:
+    CORS_ALLOWED_ORIGINS = [os.environ.get("CLIENT_ORIGIN")]
+
+if "CLIENT_ORIGIN_DEV" in os.environ:
+    extracted_url = re.match(
+        r"^.+-", os.environ.get("CLIENT_ORIGIN_DEV", ""), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 
 # Application definition
