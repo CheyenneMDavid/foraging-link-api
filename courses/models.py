16048e4ac510c63fa6b136cefb2d305c12dddf30
Chey <ctenmanikdavid@gmail.com>
2024-01-31 00:50:19 +0000

    Updated courses model

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,6 @@
 """
 Purpose of the Course model is to store information about courses offered
-within the main application
+within the main application.
 """
 
 from django.db import models
@@ -19,19 +19,39 @@ class Course(models.Model):
         ("Autumn", "Autumn"),
     ]
 
+    # Using dropdown predefined course durations.
+    DURATION_CHOICES = [
+        ("", "Select Duration"),
+        ("Half Day", "Half Day"),
+        ("Full Day", "Full Day"),
+    ]
+
+    # Always will be presented with three seasons.
     season = models.CharField(
         max_length=25, choices=SEASON_CHOICES, default="", blank=False
     )
 
     # Course title, independent, different each time.
     title = models.CharField(max_length=255)
+
     # Only the Date field required.
     date = models.DateField()
+
     # Large undefined text field
     description = models.TextField()
+
     location = models.CharField(max_length=255)
-    max_capacity = models.PositiveIntegerField()
-    duration = models.CharField(max_length=20, default="10hrs", editable=False)
+
+    # Preset and fixed number because this will never change.
+    max_capacity = models.PositiveIntegerField(default=10)
+
+    # Always will be presented with either a half-day or a full-day.
+    duration = models.CharField(
+        max_length=25,
+        choices=DURATION_CHOICES,
+        default="",
+        blank=False,
+    )
 
     # pylint: disable=invalid-str-returned
     def __str__(self):
