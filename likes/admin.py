1a39f1537e0e90e402e0fd22020fa76adc13c21a
Chey <ctenmanikdavid@gmail.com>
2024-02-05 23:19:48 +0000

    Added functionality to admin panel for likes app

--- likes/admin.py
+++ likes/admin.py
@@ -1,4 +1,25 @@
+"""
+Admin configuration to register the model with the Django admin interface,
+allowing administrators to manage likes from the admin panel.
+"""
 from django.contrib import admin
 from .models import Like
 
-admin.site.register(Like)
+
+class LikeAdmin(admin.ModelAdmin):
+    """
+    Administration for Like models, defining the list view, search capability, and filters.
+    """
+
+    # Fields to display in the admin interface
+    list_display = ("post", "owner", "created_at")
+
+    # Fields on which search functionality is enabled
+    search_fields = ["post__title", "owner__username"]
+
+    # Filters to apply in the admin list view
+    list_filter = ("created_at",)
+
+
+# Registering the Like model with LikeAdmin configuration
+admin.site.register(Like, LikeAdmin)
