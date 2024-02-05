f592844b0ab3b956c85a8499ddaf282982c3ce97
Chey <ctenmanikdavid@gmail.com>
2024-02-05 22:41:33 +0000

    Add functionality for posts app to admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -6,4 +6,22 @@ allowing administrators to manage user post from the admin panel.
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Administration object for Post models.
+    Defining the fields for list_display, search fields and the list_filter
+    """
+
+    # List of fields that will be displayed in the admin panel.
+    list_display = ("title", "owner", "created_at", "updated_at")
+
+    # Search fields for the additional functionality.
+    search_fields = ["title", "content"]
+
+    # Fields that are used to filter the searches.
+    list_filter = ("created_at", "updated_at")
+
+
+# Registering the PostAdmin configuration
+admin.site.register(Post, PostAdmin)
