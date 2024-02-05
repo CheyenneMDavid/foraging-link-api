bab757dfb75ea5723f05be805bf159c9bf9e6583
Chey <ctenmanikdavid@gmail.com>
2024-02-05 22:57:31 +0000

    Add functionality for comments app to admin panel

--- comments/admin.py
+++ comments/admin.py
@@ -6,4 +6,22 @@ allowing administrators to manage user comments from the admin panel.
 from django.contrib import admin
 from .models import Comment
 
-admin.site.register(Comment)
+
+class CommentAdmin(admin.ModelAdmin):
+    """
+    Administration object forr the Comment models with settings for the list
+    display, search with filters.
+    """
+
+    # Fields that will be displayed in the admin panel.
+    list_display = ("post", "owner", "created_at", "updated_at")
+
+    # Enables the search functionality for fields
+    search_fields = ["content", "owner__username"]
+
+    # Field that are used to filter the searches.the list view
+    list_filter = ("created_at", "updated_at")
+
+
+# Registering the CommentAdmin.
+admin.site.register(Comment, CommentAdmin)
